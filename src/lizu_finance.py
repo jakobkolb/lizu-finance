79441592670cfba2160a127e776446e5f8065322
Jakob J. Kolb <jakob.j.kolb@gmail.com>
2021-05-31 14:05:47 +0000

    Add basic auth.

--- src/lizu_finance.py
+++ src/lizu_finance.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from bokeh.embed import server_document
 from flask.templating import render_template
+from flask_basicauth import BasicAuth
 
 app = Flask(__name__)
 
+app.config['BASIC_AUTH_USERNAME'] = 'lizu'
+app.config['BASIC_AUTH_PASSWORD'] = 'S_Birkenstein'
+
+basic_auth = BasicAuth(app)
+
+
 @app.route("/")
+@basic_auth.required
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
     # tag = server_document(url='/bokeh', relative_urls=True)
