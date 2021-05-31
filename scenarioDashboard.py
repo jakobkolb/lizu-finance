e969303fa595ce61d26db65f4a12209e93b3c221
Jakob J. Kolb <jakob.j.kolb@gmail.com>
2021-05-31 16:41:02 +0000

    Setup to run bokeh server behind reverse proxy with basic auth.

--- scenarioDashboard.py
+++ scenarioDashboard.py
@@ -2,7 +2,7 @@ import gspread
 import pandas as pd
 import pandas_bokeh
 from bokeh.layouts import column, row
-from bokeh.plotting import figure
+from bokeh.plotting import figure, curdoc
 from bokeh.server.server import Server
 
 from src.helpers import connectToSheet
@@ -22,8 +22,7 @@ plot = figure(
     sizing_mode="stretch_both",
 )
 
-def run(doc):
-    doc.add_root(
+curdoc().add_root(
         column(
             row(
                 createScenario(sheet, plot, color="green", label="sc1"),
@@ -35,11 +34,3 @@ def run(doc):
             sizing_mode="scale_width",
         )
     )
-
-# configure and run bokeh server
-kws = {'port': 5100, 'prefix': '/bokeh', 'allow_websocket_origin': ['167.172.166.169']}
-server = Server(run, **kws)
-server.start()
-if __name__ == '__main__':
-    server.io_loop.add_callback(server.show, '/')
-    server.io_loop.start()
